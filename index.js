2a12cf7d1eb8a390426d2487155250b2fc84d679
shriram24P <patil.shriram98@gmail.com>
2023-01-24 22:16:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let flag = 0;
+
+function controller(x) {
+    flag = flag + x;
+    slideShow(flag);
+}
+
+slideShow(flag);
+function slideShow(num) {
+    let slides = document.getElementsByClassName('slide');
+
+    if(num == slides.length){
+        flag=0;
+        num=0;
+    }
+
+    if(num < 0) {
+        flag = slides.length-1;
+        num= slides.length-1;
+    }
+    
+    for(let elem of slides) {
+        elem.style.display = 'none';
+    }
+
+    slides[num].style.display = 'block';
+}
